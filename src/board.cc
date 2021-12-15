8f0f9d3d0d6a0dce255c0752099f27b8fe4774c4
p242chen <p242chen@student.cs.uwaterloo.ca>
2021-12-15 00:15:14 -0500

    added comments to getValidMoves and fixed some implementation errors

--- src/board.cc
+++ src/board.cc
@@ -42,46 +42,59 @@ void Board::finishTurn() {
   renderObservers();
 }
 
+
 vector<string> Board::getValidMoves() {
   vector<string> validMoves;
+  // Determines which colour's turn it is
   ColourEnum turnColour;
   if (isWhiteTurn) {
     turnColour = ColourEnum::White;
   } else {
     turnColour = ColourEnum::Black;
   }
+  // Iterates through every square on the board
   for (char col = 'a'; col < cols + 97; ++col) {
     for (int row = 1; row <= rows; ++row) {
+      // Gets the coordinate of the square
       const string coord = string(1, col) + to_string(row);
+      // Checks if there's a piece on the square that belongs to the current player
       Piece *piece = squares[coord].getPiece();
       if (piece != nullptr && piece.getColour() == turnColour) {
         const PieceEnum pieceEnum = piece->getPieceType();
+        // Gets all the potential moves of the piece at coord
         vector<vector<string>> allMoves = piece->getValidMoves();
+        // Filters out all the non-valid moves
         switch(pieceEnum) {
         case Q:
         case q:
         case R:
         case r:
         case B:
-        case b:          
+        case b:      
+          // Iterates through all the possible moves    
           for (auto moves : allMoves) {
             for (auto move : moves) {
               Piece *othPiece = squares[move].getPiece();
+              // Checks if the square is empty
               if (othPiece != nullptr) {
+                // Checks if the piece at the square is takeable
                 if (othPiece.getColour() != turnColour) {
-                  validMoves.emplace_back(move);
+                  validMoves.emplace_back(coord + " " + move);
                 }
                 break;
               } else {
-                validMoves.emplace_back(move);
+                validMoves.emplace_back(coord + " " + move);
               }
             }
           }
           break;
         case N:
         case n:
+          // Iterates through all the possible moves
           for (auto moves : allMoves) {
-            validMoves.insert(validMoves.end(), moves.begin(), moves.end());
+            for (auto move : moves) { 
+              validMoves.emplace_back(coord + " " + move);
+            }
           }
           break;
         case P:
@@ -89,23 +102,27 @@ vector<string> Board::getValidMoves() {
           for (auto moves : allMoves) {
             for (auto move : moves) {
               Piece *othPiece = squares[move].getPiece();
+              // Checks if the square is empty
               if (othPiece == nullptr) {
-                validMoves.emplace_back(move);
+                validMoves.emplace_back(coord + " " + move);
               }
             }
           }
           break;
         case K:
         case k:
+          // Iterates through all the possible moves
           for (auto moves : allMoves) {
             for (auto move : moves) {
               Piece *othPiece = squares[move].getPiece();
+              // Checks if the square is empty
               if (othPiece != nullptr) {
+                // Checks if the piece at the square is takeable
                 if (othPiece.getColour() != turnColour) {
-                  validMoves.emplace_back(move);
+                  validMoves.emplace_back(coord + " " + move);
                 }
               } else {
-                validMoves.emplace_back(move);
+                validMoves.emplace_back(coord + " " + move);
               }
             }
           }
